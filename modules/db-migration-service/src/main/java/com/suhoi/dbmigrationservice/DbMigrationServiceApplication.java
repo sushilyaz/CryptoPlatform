a5f1b6d54892fe7a42c98d7c103aef7d52856b5e
suhoi <suhoio2k00@gmail.com>
2025-10-21 20:50:15 +0300

    миграции перенес в другой сервис и все ок отработало

--- modules/db-migration-service/src/main/java/com/suhoi/dbmigrationservice/DbMigrationServiceApplication.java
+++ modules/db-migration-service/src/main/java/com/suhoi/dbmigrationservice/DbMigrationServiceApplication.java
@@ -1,13 +1,27 @@
 package com.suhoi.dbmigrationservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DbMigrationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DbMigrationServiceApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner exitAfterLiquibase(@Value("${migration.exit-after-run:true}") boolean exit) {
+        return args -> {
+            if (exit) {
+                // Если Liquibase прошёл успешно — контекст поднялся → выходим c 0
+                System.out.println("Liquibase migrations applied. Exiting as requested.");
+                System.exit(0);
+            }
+        };
     }
 
 }
