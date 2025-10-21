2612ff8be09a2a07361430547292f082d5eb7d7f
suhoi <suhoio2k00@gmail.com>
2025-10-21 19:42:32 +0300

    НАКОНЕЦ ТО ДОБАВИЛ МОДУЛЬ С БД

---
+++ modules/db-migration-service/src/main/java/com/suhoi/dbmigrationservice/DbMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suhoi.dbmigrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMigrationServiceApplication.class, args);
+    }
+
+}
